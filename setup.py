572b09a2ebd2793a9ed83efaa394bd91db86e39c
Chetan <chetanfern@gmail.com>
2024-11-20 15:29:44 +0530

    EDA_Model Training

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path:str)->List[str]:
         return requirements
 
 setup(
-      name = 'Phishing Classifier',
+      name = 'Diamond_Prediction',
       version = '0.0.1',
       author = 'Chetan',
       author_email='chetanfern@gmail.com',
